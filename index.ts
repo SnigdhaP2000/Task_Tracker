f95edf216ceee86a92a6b815f98e51b7f2eacc49
SnigdhaPatwatkar <smpatwatkar2000@gmail.com>
2025-05-01 10:46:26 +0530

    update server port to 3001 and log dynamic port in startup message

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
 import express from 'express';
 import taskRoutes from './src/routes/taskRoutes';
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(express.json());
 
 app.use("/api", taskRoutes);
 
 app.listen(port, ()=>{
-    console.log('task tracker server running on port 3000')
+    console.log(`task tracker server running on port ${port}`)
 })
 
 
